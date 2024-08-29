d58bcd7d662367a2b62865efdd8829dfffbf418d
zaedyn <yamcrunchy@gmail.com>
2024-08-28 19:01:33 -0700

    version 1.0

--- main.py
+++ main.py
@@ -33,31 +33,30 @@ wiki_index = 0
 wiki_response = ""
 wiki_chunkcount = 0
 # Tkinter
-class Window:
+class Window: # GUI (Window, label, kill and pause button)
     def __init__(self):
         self.root = tk.Tk()
 
         self.root.geometry("500x500")
-
-        self.title = tk.Label(self.root, text="Scratchpedia GUI", font=("Arial", 20))
+        self.root.configure(background='white')
+        self.title = tk.Label(self.root, text="Scratchpedia GUI", font=("Arial", 20), background="white")
         self.title.pack(pady=5)
 
         self.buttonframe = tk.Frame(self.root)
-
         self.buttonframe.columnconfigure(0, weight=1)
 
         self.killBtn = tk.Button(
-            self.root, text="Kill program", font=("Arial", 14), command=self.kill
+            self.root, text="Kill program", font=("Arial", 14), command=self.kill, background="white"
         )
         self.killBtn.pack(padx=5, pady=5)
 
         self.pauseBtn = tk.Button(
-            self.root, text="Pause", font={"Arial", 8}, command=self.pause
+            self.root, text="Pause", font=("Arial", 8), command=self.pause, background="white"
         )
         self.pauseBtn.pack(padx=5, pady=10)
         self.root.mainloop()
 
-    def kill(self):
+    def kill(self): # Ends program
         self.root.destroy()
         events.stop()
         sys.exit("Program killed.")
@@ -67,7 +66,7 @@ class Window:
             self.pauseBtn.config(text="Unpause")
             events.stop()
             print("Event listenting stopped.")
-        else:
+        else: # Unpause doesn't work 
             self.pauseBtn.config(text="Pause")
             events.start(thread=True)
             print("Event listener now listening.")
@@ -89,6 +88,8 @@ def main():
         f.close()
         f = open("logs/profanity.txt", "w+")
         f.close()
+        f = open("logs/wiki.txt", "w+")
+        f.close()
     if scratch3.get_var(id, "operation") is not None:
         print("Successfully initialized.\nStarting event listener...")
         events.start(thread=True)
@@ -99,26 +100,31 @@ def main():
         print("Failed to connect properly. Exiting...")
 
 
+def clear_txt(location):
+    f = open(location, "r+")
+    f.seek(0)
+    f.truncate()
+
 def wiki(query, type): # Handle wiki requests
     wiki_wiki = wikipediaapi.Wikipedia(
         "Scratch Wikipedia (https://scratch.mit.edu/projects/1059644681/) (Contact: yamcrunchy@gmail.com)",
         "en",
     )
-    page_py = wiki_wiki.page(query)
+    page_py = wiki_wiki.page(query) # Query page
     wiki_response = {
         "title": page_py.title,
         "content": str(page_py.summary[0:len(str(page_py.summary))]).replace("\n", " ")
         }
-    if page_py.exists():
+    if page_py.exists(): # If page exists continue
         if type == "title":
             return page_py.title
-        elif type == "content":
+        elif type == "content": # Return content chunks
             encoded_wiki_response = encode(wiki_response["content"])
             chunks = [encoded_wiki_response[i:i + 256] for i in range(0, len(encoded_wiki_response), 256)]
             return(chunks)
         elif type == "exists":
             return True
-    else: 
+    else: # If page does not exist
         return False
 
 
@@ -139,26 +145,26 @@ def encode(val):
 def decode_list(query):
     q = Encoding.decode(query)
     result = []
-    p = 0
-    i = 0
-    while p < len(q):
-        result.append("")
-        while q[p] != "|":
-            result[i] = result[i] + q[p]
-            p += 1
-        i += 1
-        p += 1
+    temp = ""
+    
+    for char in q:
+        if char == "|":
+            result.append(temp)
+            temp = ""
+        else:
+            temp += char
 
     return result
 
 
-def encode_list(query):
-    q = list(query)
-    result = ""
-    for x in range(len(q)):
-        result = result + q[x] + "|"
 
+def encode_list(query):
+    if query is None:
+        return "Error: None"
+    
+    result = "|".join(list(query)) + "|"
     return Encoding.encode(result)
+
 
 
 def log(string, location):
@@ -221,7 +227,7 @@ def handle_operation(event):
 
         elif operation_vars["type"] == "request" and operation_vars["change"] != change: # If request is for wiki access
             status = "responding to wiki access request"
-            log(f"{operation_vars['change']} Received Request @ {operation_vars['time']} from user {operation_vars['username']}", "updates")
+            log(f"{operation_vars['change']} Received Request @ {operation_vars['time']} from user {operation_vars['username']} \n", "updates")
             print(f"{operation_vars['change']} Received Request @ {operation_vars['time']} from user {operation_vars['username']}")
             change = operation_vars["change"]
             if profanity.contains_profanity(operation_vars['request']): # If request was profane log and restrict
@@ -256,7 +262,6 @@ def handle_operation(event):
                     conn.set_var("operation_response", encode_list(operation_response.values())) 
                     conn.set_var("content", wiki_response[wiki_index])
                     conn.set_var("index", "0")
-
                     log(f'{operation_vars["change"]} "{operation_vars["request"]}" pulled from wiki @ {operation_vars["time"]} by user {operation_vars["username"]}\n', "query")
                     
         elif operation_vars["type"] == "wiki-update" and status == "wiki-busy":
@@ -264,6 +269,9 @@ def handle_operation(event):
             if wiki_chunkcount <= wiki_index:
                 status = "free"
                 wiki_index = 0
+                log_msg = f'{operation_vars["change"]} Finished query "{operation_vars["request"]}" @ {operation_vars["time"]} from {operation_vars["username"]}'
+                log(log_msg + "\n", "updates")
+                print(log_msg)
             else: 
                 wiki_index+=1
                 conn.set_var("content", wiki_response[wiki_index])
@@ -276,7 +284,7 @@ def handle_operation(event):
                 "username": operation_vars["username"],
                 "days-since-2000": operation_vars["days-since-2000"],
                 "msg": msg,
-                "error": "Error 400: Bad request",
+                "error": "Error 400: Bad request"
             }
             conn.set_var("operation_response", encode_list(operation_response.values()))
             status = "free"
